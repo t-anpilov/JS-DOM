69f6b4afe09e5ed868120fe8a69be8425550de35
Taras Anpilov <t.anpilov@gmail.com>
2019-05-15 14:52:52 +0300

    First commit

---
+++ train.js
@@ -0,0 +1,161 @@
+var data = '[{"trainNumber":"255Г","startPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"destinationPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"startDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"add":{"id":0,"uaText":"Неділя","enText":"Sunday"},"finishDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"addEnd":{"id":0,"uaText":"Неділя","enText":"Sunday"},"startHour":14,"startMin":"00","finishHour":18,"finishMin":46,"price":191.97},{"trainNumber":"781М","startPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"destinationPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"startDay":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"add":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"finishDay":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"addEnd":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"startHour":14,"startMin":30,"finishHour":20,"finishMin":20,"price":234.8},{"trainNumber":"744Ч","startPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"destinationPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"startDay":{"id":1,"uaText":"Понеділок","enText":"Monday"},"add":{"id":1,"uaText":"Понеділок","enText":"Monday"},"finishDay":{"id":1,"uaText":"Понеділок","enText":"Monday"},"addEnd":{"id":1,"uaText":"Понеділок","enText":"Monday"},"startHour":18,"startMin":50,"finishHour":21,"finishMin":50,"price":121.18},{"trainNumber":"477Ф","startPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"destinationPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"startDay":{"id":3,"uaText":"Середа","enText":"Wednesday"},"add":{"uaText":"Завтра","enText":"Tomorrow"},"finishDay":{"id":4,"uaText":"Четвер","enText":"Thursday"},"addEnd":{"id":4,"uaText":"Четвер","enText":"Thursday"},"startHour":21,"startMin":30,"finishHour":"08","finishMin":37,"price":447.5},{"trainNumber":"613Н","startPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"destinationPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"startDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"add":{"id":0,"uaText":"Неділя","enText":"Sunday"},"finishDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"addEnd":{"id":0,"uaText":"Неділя","enText":"Sunday"},"startHour":15,"startMin":20,"finishHour":21,"finishMin":38,"price":253.8},{"trainNumber":"665Ш","startPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"destinationPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"startDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"add":{"id":0,"uaText":"Неділя","enText":"Sunday"},"finishDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"addEnd":{"id":0,"uaText":"Неділя","enText":"Sunday"},"startHour":"07","startMin":30,"finishHour":13,"finishMin":16,"price":232.15},{"trainNumber":"645Г","startPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"destinationPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"startDay":{"id":1,"uaText":"Понеділок","enText":"Monday"},"add":{"id":1,"uaText":"Понеділок","enText":"Monday"},"finishDay":{"id":1,"uaText":"Понеділок","enText":"Monday"},"addEnd":{"id":1,"uaText":"Понеділок","enText":"Monday"},"startHour":11,"startMin":"00","finishHour":18,"finishMin":25,"price":298.9},{"trainNumber":"667М","startPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"destinationPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"startDay":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"add":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"finishDay":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"addEnd":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"startHour":"06","startMin":35,"finishHour":11,"finishMin":44,"price":207.31},{"trainNumber":"284З","startPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"destinationPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"startDay":{"id":6,"uaText":"Субота","enText":"Saturday"},"add":{"id":6,"uaText":"Субота","enText":"Saturday"},"finishDay":{"id":6,"uaText":"Субота","enText":"Saturday"},"addEnd":{"id":6,"uaText":"Субота","enText":"Saturday"},"startHour":11,"startMin":10,"finishHour":18,"finishMin":23,"price":290.82},{"trainNumber":"467Ф","startPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"destinationPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"startDay":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"add":{"uaText":"Сьогодні","enText":"Today"},"finishDay":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"addEnd":{"uaText":"Сьогодні","enText":"Today"},"startHour":"00","startMin":"00","finishHour":"04","finishMin":51,"price":195.22},{"trainNumber":"682И","startPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"destinationPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"startDay":{"id":1,"uaText":"Понеділок","enText":"Monday"},"add":{"id":1,"uaText":"Понеділок","enText":"Monday"},"finishDay":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"addEnd":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"startHour":18,"startMin":50,"finishHour":"00","finishMin":15,"price":218.25},{"trainNumber":"498Й","startPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"destinationPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"startDay":{"id":1,"uaText":"Понеділок","enText":"Monday"},"add":{"id":1,"uaText":"Понеділок","enText":"Monday"},"finishDay":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"addEnd":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"startHour":22,"startMin":45,"finishHour":"04","finishMin":"00","price":211.41},{"trainNumber":"536Л","startPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"destinationPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"startDay":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"add":{"uaText":"Сьогодні","enText":"Today"},"finishDay":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"addEnd":{"uaText":"Сьогодні","enText":"Today"},"startHour":19,"startMin":20,"finishHour":22,"finishMin":"00","price":107.46},{"trainNumber":"621О","startPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"destinationPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"startDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"add":{"id":0,"uaText":"Неділя","enText":"Sunday"},"finishDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"addEnd":{"id":0,"uaText":"Неділя","enText":"Sunday"},"startHour":14,"startMin":25,"finishHour":20,"finishMin":50,"price":258.59},{"trainNumber":"292Е","startPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"destinationPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"startDay":{"id":4,"uaText":"Четвер","enText":"Thursday"},"add":{"id":4,"uaText":"Четвер","enText":"Thursday"},"finishDay":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"addEnd":{"id":5,"uaText":"П\'ятниця","enText":"Friday"},"startHour":18,"startMin":20,"finishHour":"07","finishMin":10,"price":517.11},{"trainNumber":"783Р","startPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"destinationPoint":{"id":"4","cityNameEn":"Dnipro","cityNameUa":"Дніпро"},"startDay":{"id":1,"uaText":"Понеділок","enText":"Monday"},"add":{"id":1,"uaText":"Понеділок","enText":"Monday"},"finishDay":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"addEnd":{"id":2,"uaText":"Вівторок","enText":"Tuesday"},"startHour":15,"startMin":"00","finishHour":"02","finishMin":"07","price":447.5},{"trainNumber":"815П","startPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"destinationPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"startDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"add":{"id":0,"uaText":"Неділя","enText":"Sunday"},"finishDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"addEnd":{"id":0,"uaText":"Неділя","enText":"Sunday"},"startHour":"03","startMin":40,"finishHour":13,"finishMin":52,"price":410.79},{"trainNumber":"489З","startPoint":{"id":"2","cityNameEn":"Lviv","cityNameUa":"Львів"},"destinationPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"startDay":{"id":3,"uaText":"Середа","enText":"Wednesday"},"add":{"uaText":"Завтра","enText":"Tomorrow"},"finishDay":{"id":3,"uaText":"Середа","enText":"Wednesday"},"addEnd":{"uaText":"Завтра","enText":"Tomorrow"},"startHour":"04","startMin":30,"finishHour":"09","finishMin":45,"price":211.41},{"trainNumber":"169В","startPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"destinationPoint":{"id":"3","cityNameEn":"Odessa","cityNameUa":"Одеса"},"startDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"add":{"id":0,"uaText":"Неділя","enText":"Sunday"},"finishDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"addEnd":{"id":0,"uaText":"Неділя","enText":"Sunday"},"startHour":"04","startMin":10,"finishHour":12,"finishMin":36,"price":339.47},{"trainNumber":"673Ч","startPoint":{"id":"1","cityNameEn":"Kharkiv","cityNameUa":"Харків"},"destinationPoint":{"id":"0","cityNameEn":"Kyiv","cityNameUa":"Київ"},"startDay":{"id":6,"uaText":"Субота","enText":"Saturday"},"add":{"id":6,"uaText":"Субота","enText":"Saturday"},"finishDay":{"id":0,"uaText":"Неділя","enText":"Sunday"},"addEnd":{"id":0,"uaText":"Неділя","enText":"Sunday"},"startHour":23,"startMin":25,"finishHour":"04","finishMin":23,"price":200.43}]';
+
+var schedule = JSON.parse(data);
+
+var tableBody = document.getElementsByTagName('tbody')[0];
+var btn =  document.getElementById('lang');
+
+var htmlUaOutput = function(item) {
+        var headRow = document.getElementsByClassName('uaHead')[0]; 
+        headRow.classList.remove('hide');        
+        var row = document.createElement('tr');
+        tableBody.appendChild(row);
+        for (var i=0; i<headRow.children.length; i++) {
+            var cell = document.createElement('td');
+            row.appendChild(cell);
+        }
+
+        row.children[0].innerHTML = item.trainNumber;
+        row.children[1].innerHTML = item.startPoint.cityNameUa + ' - ' + item.destinationPoint.cityNameUa;
+        row.children[2].innerHTML = item.startDay.uaText;
+        row.children[3].innerHTML = item.add.uaText + ' - ' + item.startHour + ' : ' + item.startMin;
+        row.children[3].setAttribute('data-day', item.startDay.id); 
+        row.children[4].innerHTML = item.addEnd.uaText + ' - ' + item.finishHour + ' : ' + item.finishMin;
+        row.children[4].setAttribute('data-day', item.finishDay.id);
+        row.children[5].innerHTML = item.price;        
+}
+
+var htmlEnOutput = function(item) {
+    var headRow = document.getElementsByClassName('enHead')[0];
+    headRow.classList.remove('hide');
+    var row = document.createElement('tr');
+    tableBody.appendChild(row);
+    for (var i=0; i<headRow.children.length; i++) {
+        var cell = document.createElement('td');
+        row.appendChild(cell);
+    }    
+    row.children[0].innerHTML = item.trainNumber;
+    row.children[1].innerHTML = item.startPoint.cityNameEn + ' - ' + item.destinationPoint.cityNameEn;
+    row.children[2].innerHTML = item.startDay.enText;
+    row.children[3].innerHTML = item.add.enText + ' - ' + item.startHour + ' : ' + item.startMin;
+    row.children[3].setAttribute('data-day', item.startDay.id); 
+    row.children[4].innerHTML = item.addEnd.enText + ' - ' + item.finishHour + ' : ' + item.finishMin;
+    row.children[4].setAttribute('data-day', item.finishDay.id);
+    row.children[5].innerHTML = item.price;   
+} 
+
+var chooseLang = confirm('Show schedule in English?');
+
+if (chooseLang === false) {
+    for (var i=0; i<schedule.length; i++) {
+        htmlUaOutput(schedule[i]);
+    }
+    btn.addEventListener('click', enLang);
+} else {
+    for (var i=0; i<schedule.length; i++) {
+        htmlEnOutput(schedule[i]);
+    }
+    btn.addEventListener('click', uaLang);
+}
+
+function uaLang() {
+    var headRow = document.getElementsByClassName('enHead')[0]; 
+    headRow.classList.add('hide');
+    clear(tableBody);
+    for (var i=0; i<schedule.length; i++) {
+        htmlUaOutput(schedule[i]);
+    }
+    btn.removeEventListener('click', uaLang);
+    btn.addEventListener('click', enLang);
+}
+
+function enLang() {
+    var headRow = document.getElementsByClassName('uaHead')[0]; 
+    headRow.classList.add('hide');
+    clear(tableBody);
+    for (var i=0; i<schedule.length; i++) {
+        htmlEnOutput(schedule[i]);
+    }
+    btn.removeEventListener('click', enLang);
+    btn.addEventListener('click', uaLang);
+}
+
+function clear(elem) {
+    while (elem.lastChild) {
+        elem.removeChild(elem.lastChild);
+    }
+}
+
+var tHead = document.getElementsByTagName('thead')[0];
+tHead.addEventListener('click', sorting);
+
+function sorting(event) {
+    var target = event.target;
+    if (target.tagName != 'SPAN') return;
+
+    sortRows(target.parentElement.cellIndex, target.getAttribute('data-type'));
+}    
+
+function sortRows (colNum, type) {
+    var startArr = [].slice.call(tableBody.rows);
+    
+
+    var compareCode = function(rowA, rowB) {
+        return parseInt(rowA.cells[colNum].innerHTML) - parseInt(rowB.cells[colNum].innerHTML);
+    }
+
+    var compareText = function(rowA, rowB) {
+        if (rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML) {
+            return 1;
+        } else if (rowB.cells[colNum].innerHTML > rowA.cells[colNum].innerHTML) {
+            return -1;
+        } else {
+            return 0;
+        }                
+    }
+
+    var compareDate = function(rowA, rowB) {
+        function getDate (timecell) {
+            var date;
+            if (timecell.getAttribute('data-day') == 0) {
+                date = 7;
+            } else {
+                date = timecell.getAttribute('data-day');
+            }
+            var hour = +timecell.innerHTML.slice(-7, -6);
+            var min = +timecell.innerHTML.slice(-2, -1);
+            return new Date(1970, 0, date, hour, min);
+        }
+        return getDate(rowA.cells[colNum]) - getDate(rowB.cells[colNum]);
+    }
+                    
+    switch (type) {
+        case 'number_down' :
+            startArr.sort(compareCode); 
+            break; 
+        case 'number_up' :
+            startArr.sort(compareCode);
+            startArr.reverse(); 
+            break;    
+        case 'text_down' :
+            startArr.sort(compareText); 
+            break;
+        case 'text_up' :
+            startArr.sort(compareText); 
+            startArr.reverse();
+            break;
+        case 'time_down' :
+            startArr.sort(compareDate); 
+            break;
+        case 'time_up' :
+            startArr.sort(compareDate); 
+            startArr.reverse();
+            break;      
+    }
+
+    clear(tableBody);
+
+    for (var i=0; i<startArr.length; i++) {
+        tableBody.appendChild(startArr[i]);
+    }    
+}
